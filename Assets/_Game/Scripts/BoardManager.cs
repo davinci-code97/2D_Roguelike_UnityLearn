f1be56d514b81f574b9d0e653a47e4ebe6a96387
Đức Anh Trần <110408669+davinci-code97@users.noreply.github.com>
2026-02-22 20:52:11 +0700

    Add Game board

--- Assets/_Game/Scripts/BoardManager.cs
+++ Assets/_Game/Scripts/BoardManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-   
+    public class CellData {
+        public bool Passable;
+    }
+
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private float boardWidth;
+    [SerializeField] private float boardHeight;
+    [SerializeField] private Tile[] groundTileArray;
+    [SerializeField] private Tile[] wallTileArray;
+
+    private Tile tile;
+    private CellData[,] BoardData;
 
     void Start()
     {
-        
+        OnInit();
     }
 
     
@@ -16,4 +28,22 @@ public class BoardManager : MonoBehaviour
     {
         
     }
+
+    void OnInit() {
+        for (int y = 0; y < boardHeight; y++) {
+            for (int x = 0; x < boardWidth; x++) {
+                if (x == 0 || y == 0 || x == boardWidth - 1 || y == boardHeight - 1) {
+                    tile = wallTileArray[Random.Range(0, wallTileArray.Length)];
+                    BoardData[x, y].Passable = false;
+                }
+                else {
+                    tile = groundTileArray[Random.Range(0, groundTileArray.Length)];
+                    BoardData[x, y].Passable = true;
+                }
+                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+            }
+        }
+    }
+
+
 }
